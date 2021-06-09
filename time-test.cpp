68f0f5088e3cf4654576488e4a7affc815f961ab
jinhaworld <jinhaworld@gmail.com>
2021-06-09 23:27:11 +0900

    initial commit

---
+++ time-test.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#include <chrono> 
+
+using namespace std; 
+
+#pragma warn -rvl 
+#pragma warn -rch 
+#pragma warn -par 
+
+int solve () {
+    return 0; 
+}
+
+int main( ) { 
+    auto start = chrono::high_resolution_clock::now();
+    /* 
+        solve(); 
+        enter code or via function 
+    */  
+    int a = 5, b = 3; 
+    auto end = chrono::high_resolution_clock::now();
+    
+    long long int ans = chrono::duration_cast<chrono::microseconds> (end - start).count(); 
+
+    cout << setprecision(10e5) << (float) ans  << " microseconds" << "\n"; 
+    cout << setprecision(10e6) << (float) ans /(10e6) << " seconds"; 
+} 
